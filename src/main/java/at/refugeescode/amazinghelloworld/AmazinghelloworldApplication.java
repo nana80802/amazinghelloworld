b1e4b41e9d43156ec2acf52c90571bde8e97035b
nana80802 <miss_nisreen2000@yahoo.com>
2018-03-21 15:30:07 +0100

    generate a spring project

---
+++ src/main/java/at/refugeescode/amazinghelloworld/AmazinghelloworldApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.amazinghelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinghelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazinghelloworldApplication.class, args);
+	}
+}
